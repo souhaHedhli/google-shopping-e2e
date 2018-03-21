4a34bce803b7779f4318a34ddab66bf5fc7b8885
Souha Hedhli <souha.hedhli@gmail.com>
2018-03-21 14:56:19 +0100

    v2

--- features/googleshopping.feature
+++ features/googleshopping.feature
@@ -3,40 +3,40 @@ Feature: Google shopping filters
   As a user on google shopping
   I want to be able to filter my results
 
-  Scenario: I can find items less than $40
+  Scenario: I can find items less than $25
     Given I am on the google shopping page
-    And I search for "Marmite"
+    When I search for "Marmite"
     Then I get "Marmite" results
-    When I click on up to $40
-    Then None of the results are more than $40
+    When I click on up to $25
+    Then None of the results are more than $25
 
   Scenario: I can find only books
     Given I am on the google shopping page
-    And I search for "Marmite"
+    When I search for "Marmite"
     Then I get "Marmite" results
     When I click on books
     Then the results are all books
 
   Scenario: I can find products sold by BritishCornerShop.co.uk
     Given I am on the google shopping page
-    And I search for "Marmite"
+    When I search for "Marmite"
     Then I get "Marmite" results
     When I click on BritishCornerShop.co.uk
     Then the results are for BritishCornerShop.co.uk
 
   Scenario: I can find products sold by Wayfair.ca
     Given I am on the google shopping page
-    And I search for "Marmite"
+    When I search for "Marmite"
     Then I get "Marmite" results
     When I click more
-    And I click on Wayfair.ca
+    When I click on Wayfair.ca
     Then the results are for Wayfair.ca
 
   Scenario: I can find products between $20 and $30
     Given I am on the google shopping page
-    And I search for "Marmite"
+    When I search for "Marmite"
     Then I get "Marmite" results
     When I enter $20
-    And I enter $30
-    And I click go
+    When I enter $30
+    When I click go
     Then the results are all between $20 and $30
--- features/stepsDefinition/steps.js
+++ features/stepsDefinition/steps.js
@@ -1,90 +1,95 @@
-const { Given, When, Then } = require('cucumber')
-var assert = chai.assert;
+const { Given, When, Then, After } = require('cucumber');
+const { assert } = require('chai');
+const config = require('../../pageobject/config')
+
+After(function () {
+  this.baseFunctions.quit();
+})
 
 Given(/^I am on the google shopping page$/, function() {
- this.baseFunctions.driver.visit(homeUrl);
+  this.baseFunctions.visit(config.homeUrl);
 })
-And(/^I search for "([^"]*)"$/, async function (arg) {
-   this.baseFunctions.driver.writeByName('q', arg);
-   const searchBtn = await this.baseFunctions.driver.findById('gbqfb');
+When(/^I search for "([^"]*)"$/, async function (arg) {
+  this.baseFunctions.writeByName('q', arg);
+   const searchBtn = await this.baseFunctions.findById('gbqfb');
    searchBtn.click();
 })
 Then(/^I get "([^"]*)" results$/, async function (arg) {
-    const elements = await this.baseFunctions.driver.findAll('.sh-dlr__list-result .eIuuYe a');
-    elements.map(function (element) {
-      assert.include(await element.getText(), arg);
+  const elements = await this.baseFunctions.findAll('.sh-dlr__list-result .eIuuYe a');
+  elements.map(async function (element) {
+    assert.include(await element.getText(), arg);
     })
 })
-When(/^I click on up to \$40$/, async function () {
-    const radio = await this.baseFunctions.driver.find('.sh-dr__g span.JcqPK');
-    radio.click();
+When(/^I click on up to \$25$/, async function () {
+  const radio = await this.baseFunctions.find('.sh-dr__g span.JcqPK');
+  radio.click();
 })
-Then(/^None of the results are more than \$40$/, async function () {
-  const prices = await this.baseFunctions.driver.findAll('.sh-dlr__list-result .ZGFjDb .mQ35Be span.O8U6h');
+Then(/^None of the results are more than \$25$/, async function () {
+  const prices = await this.baseFunctions.findAll('.sh-dlr__list-result .ZGFjDb .mQ35Be span.O8U6h');
     prices.map(function (price) {
-      assert.isAtMost(price, '$40');
+      assert.isAtMost(price, '$25');
     })
 })
 
 When(/^I click on books$/, async function () {
-  const more = await this.baseFunctions.driver.find('g-header-menu');
+  const more = await this.baseFunctions.find('g-header-menu');
   more.click();
-  const books = await this.baseFunctions.driver.findByLinkText('Books');
+  const books = await this.baseFunctions.findByLinkText('Books');
   books.click();
 })
 
 Then(/^the results are all books$/, async function () {
-  const parentElement = await this.baseFunctions.driver.findAll('.bkWMgd')[0];
-  const childElements = await this.baseFunctions.driver.findAllInElement(parentElement, 'cite');
-  childElements.map(function (childElement) {
+  const parentElement = await this.baseFunctions.findAll('.bkWMgd')[0];
+  const childElements = await this.baseFunctions.findAllInElement(parentElement, 'cite');
+  childElements.map(async function (childElement) {
     assert.include(await childElement.getText(), 'https://books.google');
   })
 })
 
 When(/^I click on BritishCornerShop\.co\.uk$/, async function () {
-  const btn = await this.baseFunctions.driver.find('[title="BritishCornerShop.co.uk"]');
+  const btn = await this.baseFunctions.find('[title="BritishCornerShop.co.uk"]');
   btn.click();
 })
 Then(/^the results are for BritishCornerShop\.co\.uk$/, async function () {
-  const elements = await this.baseFunctions.driver.findAll('.mQ35Be div:not(.vq3ore)');
-  elements.map( function(element) {
+  const elements = await this.baseFunctions.findAll('.mQ35Be div:not(.vq3ore)');
+  elements.map(async function(element) {
     assert.include( await element.getText(), 'from BritishCornerShop.co.uk');
   })
 })
 
 When(/^I click more$/, async function () {
-  const moreBtn = await this.baseFunctions.driver.find('.ZI2vDe');
+  const moreBtn = await this.baseFunctions.find('.ZI2vDe');
   moreBtn.click();
 })
-And(/^I click on Wayfair\.ca$/, async function (arg) {
-  const btn = await this.baseFunctions.driver.find('[title="Wayfair.ca"]');
+When(/^I click on Wayfair\.ca$/, async function (arg) {
+  const btn = await this.baseFunctions.find('[title="Wayfair.ca"]');
   btn.click();
 })
 Then(/^the results are for Wayfair\.ca$/, async function () {
-  const elements = await this.baseFunctions.driver.findAll('.mQ35Be div:not(.vq3ore)');
-  elements.map( function(element) {
+  const elements = await this.baseFunctions.findAll('.mQ35Be div:not(.vq3ore)');
+  elements.map(async function(element) {
     assert.include( await element.getText(), 'Wayfair.ca');
   })
 })
 
 When(/^I enter \$20$/, async function () {
-  this.baseFunctions.driver.writeByName('lower', '20');
+  this.baseFunctions.writeByName('lower', '20');
 })
 
-And(/^I enter \$30$/, async function () {
-  this.baseFunctions.driver.writeByName('upper', '30');
+When(/^I enter \$30$/, async function () {
+  this.baseFunctions.writeByName('upper', '30');
 })
 
-And(/^I click go$/, async function () {
-  const btn = await this.baseFunctions.driver.find('.LfUe1b');
+When(/^I click go$/, async function () {
+  const btn = await this.baseFunctions.find('.LfUe1b');
   btn.click();
 })
 
 Then(/^the results are all between \$20 and \$30$/, async function () {
-  const elements = await this.baseFunctions.driver.findAll('.mQ35Be div span');
-  elements.map(function (element) {
+  const elements = await this.baseFunctions.findAll('.mQ35Be div span');
+  elements.map(async function (element) {
     const price = await parseFloat(element.getText().replace(',', '.'));
-    assert.isAtLeast(price, 20)
+    assert.isAtLeast(price, 20);
     assert.isAtMost(price, 30);
   })
 })
